2e541ba846c5acc3fcbb57e2b7dab7575411895e
jenshja <jenshja@student.uio.no>
2018-10-24 19:36:06 +0200

    Made code more efficient by implementing size. Fixed intendations.

--- linked_list.cpp
+++ linked_list.cpp
@@ -8,17 +8,16 @@ struct Node {
 	Node* next;
 };
 
-
 class LinkedList {
 private:
 	Node* head;
 	Node* tail;
+    int size; // Is equivalent to length(). Since many of the methods are dependent of the length of the linked list, using size is more efficient. 
 
 	Node* get_node(int index) {
-    	if (index < 0 || index >= length()) {
+    	if (index < 0 || index >= size) {
         	throw range_error("IndexError: Index out of range");
     	}
-
     	Node* current = head;
     	for (int i=0; i<index; i++) {
         	current = current->next;
@@ -30,28 +29,29 @@ public:
 	LinkedList() {
 		head = nullptr;
 		tail = nullptr;
+        size = 0;
 	}
 
 	LinkedList(vector<int> vec) {
 		head = nullptr;
 		tail = nullptr;
+        size = 0;
 		for (int v:vec) {
 			append(v);
 		}
 	}
 
 	~LinkedList() {
-    Node* current;
-    Node* next;
-
-    current = head;
-
-    while (current != nullptr) {
-        next = current->next;
-        delete current;
-        current = next;
-    	}
-    }
+        Node* current;
+        Node* next;
+        current = head;
+        while (current != nullptr) {
+            next = current->next;
+            delete current;
+            current = next;
+        	}
+        }
+ 
 
 	int length() {
 		Node* current = head;
@@ -70,9 +70,9 @@ public:
             n->next = nullptr;
 			head = n;
 			tail = head;
+            size += 1;
 			return;
 		}
-
 		else { 
 			Node* current;
 			current = tail;
@@ -81,67 +81,68 @@ public:
             n->next = nullptr;
 			current->next = n;
 			tail = current->next;
+            size += 1;
 			return; 
 		}
 	}
 
-		void print() {
-			Node* current = head;
-			cout << "[";
-			while (current->next !=nullptr) {
-				cout << current->value;
-				cout << ", ";
-				current = current->next;
-	    	}
-			cout << current->value << "]" << endl;	
-		}
+	void print() {
+		Node* current = head;
+		cout << "[";
+		while (current->next !=nullptr) {
+			cout << current->value;
+			cout << ", ";
+			current = current->next;
+	   	}
+		cout << current->value << "]" << endl;	
+	}
 
-		int& operator[](int index) {
-            return get_node(index)->value;
-        }
+	int& operator[](int index) {
+        return get_node(index)->value;
+    }
 
-        void insert(int val, int index) {
-        	Node* current = get_node(index-1);
-        	Node* n = new Node;
-        	n->value = val;
-        	n->next = current->next;
-        	current->next = n; 
-        }
+    void insert(int val, int index) {
+    	Node* current = get_node(index-1);
+    	Node* n = new Node;
+    	n->value = val;
+    	n->next = current->next;
+    	current->next = n;
+        size += 1; 
+    }
 
-        void remove(int index) {				//Can be made more efficient.
-        	Node* current = get_node(index);
-        	if (current->next == nullptr) {
-        		Node* temp = get_node(index-1);
-        		temp->next = nullptr;
-        		tail = temp;
-        	}
-        	else if (current == head) {
-        		head = current->next;
-        		current->next = nullptr;
-        	}
+    void remove(int index) {				//Can be made more efficient.
+    	Node* current = get_node(index);
+    	if (current->next == nullptr) {
+    		Node* temp = get_node(index-1);
+    		temp->next = nullptr;
+    		tail = temp;
+    	}
+    	else if (current == head) {
+    		head = current->next;
+    	}
+    	else {
+    		Node* temp = get_node(index-1);
+    	    temp->next = current->next;  
+        }
+        size -= 1;  
+    }
 
-        	else {
-        		Node* temp = get_node(index-1);
-        	    temp->next = current->next;
-        	    current->next = nullptr;
-            }
-        }
-        int pop(int index) {
-        	Node* temp = get_node(index);
-        	int pop_val = temp->value;
-        	remove(index);
-        	return pop_val;
-        }
+    int pop(int index) {
+    	Node* temp = get_node(index);
+    	int pop_val = temp->value;
+    	remove(index);
+    	return pop_val;
+    }
 
-        int pop() {
-        	int pop_val = tail->value;
-        	remove(length()-1);
-        	return pop_val;
-        }
+    int pop() {
+    	int pop_val = tail->value;
+    	remove(size-1);
+    	return pop_val;
+    }
 };
 
 void test_LinkedList() {
-	// Testing append
+	// Testing append(int)
 	LinkedList primes;
 	primes.append(2);
 	primes.append(3);
@@ -149,12 +150,14 @@ void test_LinkedList() {
 	primes.append(11);
 	// Testing print
 	primes.print(); 
-    // Testing []
+    // Testing length() 
+    assert(4 == primes.length());
+    // Testing the operator []
 	assert(3 == primes[1]);
-    // Testing insert 
+    // Testing insert(int,int) 
     primes.insert(7,3);
     assert(7 == primes[3]);
-    // Testing remove
+    // Testing remove(int)
     primes.remove(3);
     assert(7 != primes[3]);
     // Testing pop(int) 
@@ -166,7 +169,7 @@ void test_LinkedList() {
     // Testing LinkedList(vector<int>)
 	LinkedList primes_2({2,3,5,11});
 	primes_2.print();
-}
+}    
 
 int main() {
 	test_LinkedList();
