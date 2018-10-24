f3b1ac532f1d47e02b159c4d9e28d6b993a59939
jenshja <jenshja@student.uio.no>
2018-10-24 18:29:37 +0200

    added proper tests, fixed indentation and fixed pop()

--- array_list.cpp
+++ array_list.cpp
@@ -6,175 +6,196 @@
 using namespace std;
 
 class ArrayList {
-	private:
-		int *data;
-		int capacity;
-		int growth_factor = 2;
+private:
+	int *data;
+	int capacity;
+	int growth_factor = 2;
 
-	public:
-		int size;
-		int get_capacity() {
-			return capacity;
+	void resize(int n) {
+		capacity *= growth_factor;
+		int *tmp = new int[capacity];
+		for (int i=0; i<size; i++) {
+			tmp[i] = data[i];
 		}
+		delete[] data;
+		data = tmp;
+	}		
 
-		ArrayList()  {
-			size = 0;
-			capacity = 1;
-			data = new int[capacity];
+public:
+    int size;
+
+	int get_capacity() {
+		return capacity;
+	}
+
+	ArrayList()  {
+		size = 0;
+		capacity = 1;
+		data = new int[capacity];
+	}
+
+	ArrayList(vector<int> vect) {
+		size = 0;
+		capacity = 1;
+		data = new int[capacity];
+		for (int v:vect) {
+			append(v);
 		}
+	}
 
-		ArrayList(vector<int> vect) {
-			size = 0;
-			capacity = 1;
-			data = new int[capacity];
-			for (int v:vect) {
-				append(v);
-			}
+	~ArrayList() {
+		delete[] data;
+	}		
+
+	int length() {
+		return size;
+	}
+
+	void append(int n) {
+		if (size >= capacity) {
+			resize(n);
 		}
+		data[size] = n;
+		size += 1;
+	}
 
-		~ArrayList() {
-			delete[] data;
-		}		
-
-		int length() {
-			return size;
+	void print() {
+		cout << "[";
+		for (int i=0; i<(size-1); i++) {
+			cout << data[i] << ",";
 		}
+		cout << data[size-1] << "]" << endl;	
+	}
 
-		void append(int n) {
-			if (size >= capacity) {
-				resize(n);
-			}
-			data[size] = n;
-			size += 1;
+
+	int &operator[](int i) {
+		if (0 <= i and i < size) {
+			return data[i];
+		} else {
+			throw out_of_range("IndexError");
 		}
+	}
 
-		void resize(int n) {
-			capacity *= growth_factor;
-			int *tmp = new int[capacity];
-			for (int i=0; i<size; i++) {
+	void insert(int val, int index) {
+		int *tmp = new int[capacity];
+		if (index <= size) {
+			for (int i=0; i<index; i++) {
 				tmp[i] = data[i];
 			}
-			delete[] data;
-			data = tmp;
-		}
-
-		void print() {
-			cout << "[";
-			for (int i=0; i<(size-1); i++) {
-				cout << data[i] << ",";
+			tmp[index] = val;
+			for (int i=(index+1); i<=(size); i++) {
+				tmp[i] = data[i-1];
 			}
-
-			cout << data[size-1] << "]" << endl;	
+		} 
+		else {
+			throw out_of_range("IndexError");
 		}
+		delete[] data;
+		data = tmp;
+		size +=1;
+	}
 
 
-		int &operator[](int i) {
-			if (0 <= i and i < size) {
-				return data[i];
-			} else {
-				throw out_of_range("IndexError");
+	void remove(int index) {
+		int *tmp = new int[capacity];
+		if (index <= size) {
+			for (int i=0; i<index; i++) {
+				tmp[i] = data[i];
 			}
-		}
-
-		void insert(int val, int index) {
-			int *tmp = new int[capacity];
-			if (index <= size) {
-				for (int i=0; i<index; i++) {
-					tmp[i] = data[i];
-				}
-				tmp[index] = val;
-				for (int i=(index+1); i<=(size); i++) {
-					tmp[i] = data[i-1];
-				}
-			} 
-			else {
-				throw out_of_range("IndexError");
+			for (int i=(index+1); i<(size); i++) {
+				tmp[i-1] = data[i];
 			}
-			delete[] data;
-			data = tmp;
-			size +=1;
+		} 
+		else {
+			throw out_of_range("IndexError");
 		}
-
-
-		void remove(int index) {
-			int *tmp = new int[capacity];
-			if (index <= size) {
-				for (int i=0; i<index; i++) {
-					tmp[i] = data[i];
-				}
-				for (int i=(index+1); i<(size); i++) {
-					tmp[i-1] = data[i];
-				}
-			} 
-			else {
-				throw out_of_range("IndexError");
-			}
-			delete[] data;
-			data = tmp;
-			size -=1;
-			if (size < 0.25 * capacity) {
-				shrink_to_fit();
-			}
+		delete[] data;
+		data = tmp;
+		size -=1;
+		if (size < 0.25 * capacity) {
+			shrink_to_fit();
 		}
+	}
 
-		int pop(int index) {
-			int pop_val = data[index];
-			remove(index);
-			return pop_val;
-		}
+	int pop(int index) {
+		int pop_val = data[index];
+		remove(index);
+		return pop_val;
+	}
 
-		int pop() {
-			int pop_val = data[size-1];
-			remove(size);
-			return pop_val;
-		}
+	int pop() {
+		int pop_val = data[size-1];
+		remove(size-1);
+		return pop_val;
+	}
 
-		void shrink_to_fit() {
-			if(size == 0 || size == 1) {
-				capacity = 1;
-			}
-			else {
-				int n; 
-				n = ceil(log(size)/log(2));
-				capacity = pow(2,n);
-		    }
+	void shrink_to_fit() {
+		if(size == 0 || size == 1) {
+			capacity = 1;
 		}
+		else {
+			int n; 
+			n = ceil(log(size)/log(2));
+			capacity = pow(2,n);
+	    }
+	}
 };
 
 bool is_prime(int n) {
 	int i = 2;
 	bool val;
-	while (i < n) {
-		if (n % i == 0) {
-			val = false;
-			break;
-		}
-		else {
-			val = true;
-		}	
-		i += 1;
+	if (n == 0 || n == 1) {
+		val = false;
 	}
+	else if (n == 2) {
+		val = true;
+	}
+	else {
+		while (i < n) {
+			if (n % i == 0) {
+				val = false;
+				break;
+			}
+			else {
+				val = true;
+			}	
+			i += 1;
+		}
+    }
 	return val;	
 }
 
 void test_ArrayList() {
-	ArrayList vec;
-	for (int i=0; i<=513; i++) {
-		vec.append(i);
+
+	// Test append, remove, get_capacity (and resize - implicitly)  
+	ArrayList test_vec;
+	for (int i=0; i<=513; i++) { 
+		test_vec.append(i);
 	}
-	int cap = vec.get_capacity();
+	int cap = test_vec.get_capacity();
 	assert(cap == 1024);
-	for (int i=0; i<=257; i++) {
-		vec.remove(i);
+	for (int i=0; i<=256; i++) { // Removing all even numbers 
+		test_vec.remove(i);
 	}
-	vec.shrink_to_fit();
-	int new_cap = vec.get_capacity();
+
+	// Testing insert(), pop(int), pop(int), shrink_to_fit() and the operator []
+	test_vec.insert(707, 3);
+	assert(test_vec[3] == 707);
+
+	int test_pop1 = test_vec.pop(3);
+	assert(test_pop1 == 707);
+
+    int test_pop2 = test_vec.pop();
+    assert(test_pop2 == 513);
+
+    test_vec.shrink_to_fit();
+	int new_cap = test_vec.get_capacity();
 	assert(new_cap == 256);
 }
 
 int main() {
 	ArrayList primes;
-	for (int i=2; i<30; i++) {
+	for (int i=0; i<30; i++) {
 		if (is_prime(i) == true) {
 			primes.append(i);
 		}
